6e74bdec0e324059ddea916b719162aeb2c22bdd
Luke van der Hoeven <hungerandthirst@gmail.com>
2016-04-18 15:01:27 -0400

    script saving from dump

--- src/authoritah/rule.cr
+++ src/authoritah/rule.cr
@@ -5,7 +5,7 @@ module Authoritah
     include Mixins::DiffHelpers
     include Mixins::Serializer
 
-    serialize_with :name, :id, :enabled, :order, :stage
+    serialize_with :uuid, :name, :script_file, :stage, :enabled
     def_equals_type RuleConfig, :name, :id, :enabled, :stage, :script
 
     property :script_file
@@ -19,11 +19,17 @@ module Authoritah
       stage:   String,
     })
 
+    def uuid
+      self.id
+    end
+
     def save_script(@script_file = "./rules/#{name}.js")
       script_dir = File.dirname(@script_file.not_nil!)
       Dir.mkdir_p(script_dir) unless Dir.exists? script_dir
 
-      File.write(@script_file.not_nil!, script)
+      # add trailing newline
+      File.write(@script_file.not_nil!, script + "\n")
+      script_file
     end
   end
 end
